c7dc0cd844823897ba020a14c824ccb13a5f6132
Arenelin <moroznaya2002@gmail.com>
2024-05-06 10:09:47 +0300

    feat: adds new description for App

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload. New description.
+        </p>
       </header>
     </div>
   );
