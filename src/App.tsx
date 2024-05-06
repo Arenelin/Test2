3a4a4841eb812fabf0af478d5db764eb79ffc28c
Arenelin <moroznaya2002@gmail.com>
2024-05-06 10:31:42 +0300

    feat: changes greeting

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-        <h1>New title</h1>
-        <h1>New title</h1>
-        <h1>New title</h1>
-        <h1>New title</h1>
-        <h1>New title</h1>
-        <h1>New title</h1>
-        <h1>New title</h1>
-        <h1>New title</h1>
-        <h1>New title</h1>
-        <h1>New title</h1>
-        <h1>New title</h1>
-        <h1>New title</h1>
-        <h1>New title</h1>
-        <h1>New title</h1>
-        <h1>New title</h1>
-        <h1>New title</h1>
-        <h1>New title</h1>
-        <h1>New title</h1>
-        <h1>New title</h1>
-        <h1>New title</h1>
-        <div>new header</div>
         <div>new header version 2</div>
         <div>new header version 3</div>
     </div>
