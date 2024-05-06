f8f844b7dfacefac4927b20006b74a73e5810052
Arenelin <moroznaya2002@gmail.com>
2024-05-06 10:37:44 +0300

    feat: changes new header 2 from main

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <div>new header version 2</div>
+        <div>new header version 2242424244242424</div>
         <div>new changes from main header version 3 3 123 1231 12313</div>
     </div>
   );
