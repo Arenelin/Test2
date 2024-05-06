e3cf0752e977dbcfda1a1c274a667e4610effd12
Arenelin <moroznaya2002@gmail.com>
2024-05-06 10:28:48 +0300

    feat: removes descr

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload. New description.
-        </p>
       </header>
         <h1>New title</h1>
         <div>new header</div>
